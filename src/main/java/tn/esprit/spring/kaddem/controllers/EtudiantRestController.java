fc6a1c3277463f49c38b743aece65a90fc6353a9
mohamed amine aouididi <mohamedamine.aouididi@esprit.tn>
2023-11-04 04:03:28 +0100

    Revert "Revert "after SonarQube Test""
    This reverts commit 6565c3c1a5fd11438f0479974dec8216631db541.

--- src/main/java/tn/esprit/spring/kaddem/controllers/EtudiantRestController.java
+++ src/main/java/tn/esprit/spring/kaddem/controllers/EtudiantRestController.java
@@ -1,7 +1,5 @@
 package tn.esprit.spring.kaddem.controllers;
 
-import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import tn.esprit.spring.kaddem.entities.Etudiant;
 import tn.esprit.spring.kaddem.services.IEtudiantService;
@@ -9,16 +7,19 @@ import tn.esprit.spring.kaddem.services.IEtudiantService;
 import java.util.List;
 
 @RestController
-@AllArgsConstructor
 @RequestMapping("/etudiant")
 public class EtudiantRestController {
-	@Autowired
-	IEtudiantService etudiantService;
-	// http://localhost:8089/Kaddem/etudiant/retrieve-all-etudiants
+
+	private final IEtudiantService etudiantService;
+
+	public EtudiantRestController(IEtudiantService etudiantService) {
+		this.etudiantService = etudiantService;
+
+	}
+
 	@GetMapping("/retrieve-all-etudiants")
 	public List<Etudiant> getEtudiants() {
-		List<Etudiant> listEtudiants = etudiantService.retrieveAllEtudiants();
-		return listEtudiants;
+		return etudiantService.retrieveAllEtudiants();
 	}
 	// http://localhost:8089/Kaddem/etudiant/retrieve-etudiant/8
 	@GetMapping("/retrieve-etudiant/{etudiant-id}")
@@ -29,8 +30,7 @@ public class EtudiantRestController {
 	// http://localhost:8089/Kaddem/etudiant/add-etudiant
 	@PostMapping("/add-etudiant")
 	public Etudiant addEtudiant(@RequestBody Etudiant e) {
-		Etudiant etudiant = etudiantService.addEtudiant(e);
-		return etudiant;
+		return etudiantService.addEtudiant(e);
 	}
 
 	// http://localhost:8089/Kaddem/etudiant/remove-etudiant/1
@@ -42,9 +42,7 @@ public class EtudiantRestController {
 	// http://localhost:8089/Kaddem/etudiant/update-etudiant
 	@PutMapping("/update-etudiant")
 	public Etudiant updateEtudiant(@RequestBody Etudiant e) {
-		Etudiant etudiant= etudiantService.updateEtudiant(e);
-
-		return etudiant;
+		return etudiantService.updateEtudiant(e);
 	}
 
 	//@PutMapping("/affecter-etudiant-departement")
@@ -57,8 +55,8 @@ public class EtudiantRestController {
     @PostMapping("/add-assign-Etudiant/{idContrat}/{idEquipe}")
     @ResponseBody
     public Etudiant addEtudiantWithEquipeAndContract(@RequestBody Etudiant e, @PathVariable("idContrat") Integer idContrat, @PathVariable("idEquipe") Integer idEquipe) {
-        Etudiant etudiant = etudiantService.addAndAssignEtudiantToEquipeAndContract(e,idContrat,idEquipe);
-        return etudiant;
+		return etudiantService.addAndAssignEtudiantToEquipeAndContract(e,idContrat,idEquipe);
+
     }
 
 	@GetMapping(value = "/getEtudiantsByDepartement/{idDepartement}")
