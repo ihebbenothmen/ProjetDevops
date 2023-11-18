27ad419c06e7ef2b859fa3e35eefdf3878e3384e
mohamed amine aouididi <mohamedamine.aouididi@esprit.tn>
2023-11-18 03:22:22 +0100

    angular commit changes

--- src/main/java/tn/esprit/spring/kaddem/controllers/EtudiantRestController.java
+++ src/main/java/tn/esprit/spring/kaddem/controllers/EtudiantRestController.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/etudiant")
+@CrossOrigin("*")
 public class EtudiantRestController {
 
 	private final IEtudiantService etudiantService;
