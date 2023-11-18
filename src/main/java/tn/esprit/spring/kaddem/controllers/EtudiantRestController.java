a938c1f696a8579bc17fce31962f4eb72e10ba2f
mohamed amine aouididi <mohamedamine.aouididi@esprit.tn>
2023-11-18 06:22:26 +0100

    Update EtudiantRestController.java

--- src/main/java/tn/esprit/spring/kaddem/controllers/EtudiantRestController.java
+++ src/main/java/tn/esprit/spring/kaddem/controllers/EtudiantRestController.java
@@ -2,8 +2,6 @@ package tn.esprit.spring.kaddem.controllers;
 
 import org.springframework.web.bind.annotation.*;
 import tn.esprit.spring.kaddem.entities.Etudiant;
-import tn.esprit.spring.kaddem.entities.Option;
-import tn.esprit.spring.kaddem.entities.dto.EtudiantDTO;
 import tn.esprit.spring.kaddem.services.IEtudiantService;
 
 import java.util.List;
@@ -22,8 +20,8 @@ public class EtudiantRestController {
 	// http://localhost:8089/Kaddem/etudiant/retrieve-all-etudiants
 	@GetMapping("/retrieve-all-etudiants")
 	public List<Etudiant> getEtudiants() {
-		List<Etudiant> listEtudiants = etudiantService.retrieveAllEtudiants();
-		return listEtudiants;
+		return   etudiantService.retrieveAllEtudiants();
+
 	}
 	// http://localhost:8089/Kaddem/etudiant/retrieve-etudiant/8
 	@GetMapping("/retrieve-etudiant/{etudiant-id}")
@@ -34,8 +32,7 @@ public class EtudiantRestController {
 	// http://localhost:8089/Kaddem/etudiant/add-etudiant
 	@PostMapping("/add-etudiant")
 	public Etudiant addEtudiant(@RequestBody Etudiant e) {
-		Etudiant etudiant = etudiantService.addEtudiant(e);
-		return etudiant;
+		return etudiantService.addEtudiant(e);
 	}
 
 	// http://localhost:8089/Kaddem/etudiant/remove-etudiant/1
@@ -47,9 +44,9 @@ public class EtudiantRestController {
 	// http://localhost:8089/Kaddem/etudiant/update-etudiant
 	@PutMapping("/update-etudiant")
 	public Etudiant updateEtudiant(@RequestBody Etudiant e) {
-		Etudiant etudiant= etudiantService.updateEtudiant(e);
+		return etudiantService.updateEtudiant(e);
 
-		return etudiant;
+
 	}
 
 	//@PutMapping("/affecter-etudiant-departement")
@@ -62,8 +59,8 @@ public class EtudiantRestController {
 	@PostMapping("/add-assign-Etudiant/{idContrat}/{idEquipe}")
 	@ResponseBody
 	public Etudiant addEtudiantWithEquipeAndContract(@RequestBody Etudiant e, @PathVariable("idContrat") Integer idContrat, @PathVariable("idEquipe") Integer idEquipe) {
-		Etudiant etudiant = etudiantService.addAndAssignEtudiantToEquipeAndContract(e,idContrat,idEquipe);
-		return etudiant;
+		return etudiantService.addAndAssignEtudiantToEquipeAndContract(e,idContrat,idEquipe);
+
 	}
 
 	@GetMapping(value = "/getEtudiantsByDepartement/{idDepartement}")
